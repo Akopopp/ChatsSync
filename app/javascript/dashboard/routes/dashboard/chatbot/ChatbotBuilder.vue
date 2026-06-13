d5fd650fe46fc1c869b3ac5d09e2d4172c52d319
ChatsSync <abdullahkashifop@gmail.com>
2026-06-13 21:14:18 +0500

    Create ChatbotBuilder.vue

---
+++ app/javascript/dashboard/routes/dashboard/chatbot/ChatbotBuilder.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chatbot-container">
+    <iframe
+      :src="typebotUrl"
+      frameborder="0"
+      width="100%"
+      height="100%"
+      allow="clipboard-read; clipboard-write"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    typebotUrl() {
+      return 'https://typebot.5.75.237.171.sslip.io';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chatbot-container {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
