2fa4de9117bc056f9475292db2c3a7e4bfe7c9f9
ChatsSync <abdullahkashifop@gmail.com>
2026-06-16 12:31:42 +0500

    Create Gallery.vue

---
+++ app/javascript/dashboard/routes/dashboard/gallery/Gallery.vue
@@ -0,0 +1,14 @@
+<script setup>
+const BUILDER_URL = 'http://zpzybpz68hsj0wvbpnar61in.5.75.237.171.sslip.io';
+</script>
+
+<template>
+  <div style="width: 100%; height: 100%; display: flex; flex-direction: column;">
+    <iframe
+      :src="`${BUILDER_URL}/?view=gallery`"
+      style="width: 100%; height: 100%; border: 0;"
+      title="ChatsSync Gallery"
+      allow="clipboard-write"
+    />
+  </div>
+</template>
