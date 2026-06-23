25935b9592dc86ec40a636214ad4582c7d52f719
ChatsSync <abdullahkashifop@gmail.com>
2026-06-23 21:20:12 +0500

    Update chatbot.routes.js

--- app/javascript/dashboard/routes/dashboard/chatbot/chatbot.routes.js
+++ app/javascript/dashboard/routes/dashboard/chatbot/chatbot.routes.js
@@ -1,15 +1,13 @@
 import ChatbotBuilder from './ChatbotBuilder.vue';
 import { frontendURL } from '../../../helper/URLHelper';
-
 const routes = [
   {
     path: frontendURL('accounts/:accountId/chatbot'),
     name: 'chatbot_builder',
     meta: {
-      permissions: ['administrator', 'agent'],
+      permissions: ['administrator'],
     },
     component: ChatbotBuilder,
   },
 ];
-
 export default routes;
