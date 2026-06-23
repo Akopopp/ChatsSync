62c1b3e21314ce6ab4304ebd03b4ffb5ef77710c
ChatsSync <abdullahkashifop@gmail.com>
2026-06-23 21:17:00 +0500

    Update gallery.routes.js

--- app/javascript/dashboard/routes/dashboard/gallery/gallery.routes.js
+++ app/javascript/dashboard/routes/dashboard/gallery/gallery.routes.js
@@ -1,15 +1,13 @@
 import Gallery from './Gallery.vue';
 import { frontendURL } from '../../../helper/URLHelper';
-
 const routes = [
   {
     path: frontendURL('accounts/:accountId/gallery'),
     name: 'gallery_index',
     meta: {
-      permissions: ['administrator', 'agent'],
+      permissions: ['administrator'],
     },
     component: Gallery,
   },
 ];
-
 export default routes;
